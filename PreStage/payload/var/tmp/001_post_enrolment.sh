bdcbe759bb1b1a7cabba49ad832ac393ebd0bf24
James Smith <james@anz.com>
2022-06-02 14:57:31 +1000

    Updating post enrolment script

--- PreStage/payload/var/tmp/001_post_enrolment.sh
+++ PreStage/payload/var/tmp/001_post_enrolment.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Post-Enrolment script used at ANZ
-# Author: James Smith - james@smithjw.me
+# Author: James Smith - james@smithjw.me / james@anz.com
 # Version 3.0
 
 jamf_binary="/usr/local/jamf/bin/jamf"
 jamf_process=$(pgrep -x "jamf")
 finder_process=$(pgrep -l "Finder")
 log_folder="/private/var/log"
-log_name="enrolment.log"
+log_name="swiftEnrolment.log"
 
 echo_logger() {
     log_folder="${log_folder:=/private/var/log}"
@@ -37,13 +37,13 @@ until [ "$finder_process" != "" ]; do
     finder_process=$(pgrep -l "Finder")
 done
 
-defaults write /Library/Management/ANZ/management_info.plist enrol_login_policy_start "$(date +%s)"
+defaults write /Library/Management/management_info.plist enrol_login_policy_start "$(date +%s)"
 
 jamf_check "manage"
 jamf_check "recon"
 jamf_check "policy"
 
-defaults write /Library/Management/ANZ/management_info.plist enrol_login_policy_end "$(date +%s)"
+defaults write /Library/Management/management_info.plist enrol_login_policy_end "$(date +%s)"
 
 echo_logger "SCRIPT: Cleaning up post-enrolment script"
 rm /usr/local/outset/login-privileged-once/001_post_enrolment.sh
